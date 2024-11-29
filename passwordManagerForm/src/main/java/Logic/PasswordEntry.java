bc9b7b91e6f65d2e45a672b9a7fb7b5a07f1cbaa
Juan Manuel Torres <juanmatorresvillota@gmail.com>
2024-11-28 23:25:27 -0500

    add PasswordEntry class

--- passwordManagerForm/src/main/java/Logic/PasswordEntry.java
+++ passwordManagerForm/src/main/java/Logic/PasswordEntry.java
@@ -1,3 +1,80 @@
 package passwordManagerForm.Logic;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
 
+public class PasswordEntry {
+    private String id;
+    private String title;
+    private String username;
+    private String encryptedPassword;
+    private String url;
+    private String notes;
+
+    public PasswordEntry(String id, String title, String username, String encryptedPassword, String url, String notes) {
+        this.id = id;
+        this.title = title;
+        this.username = username;
+        this.encryptedPassword = encryptedPassword;
+        this.url = url;
+        this.notes = notes;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEncryptedPassword() {
+        return encryptedPassword;
+    }
+
+    public void setEncryptedPassword(String encryptedPassword) {
+        this.encryptedPassword = encryptedPassword;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public void encryptPassword(String password, EncryptionService encryptionService) {
+        this.encryptedPassword = encryptionService.encrypt(password);
+    }
+
+    public String decryptPassword(EncryptionService encryptionService) { return encryptionService.decrypt(this.encryptedPassword); }
+    
+}
